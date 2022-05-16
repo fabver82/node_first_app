c1c519a41a7c16ab528acd5c6888ab187f962fb2
Fabien V <fabien.vermeiren@gmail.com>
2022-05-16 14:06:11 +0200

    add DELETE, PUT and GET by email

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -34,4 +34,67 @@ module.exports = (app) => {
       res.sendStatus(200);
     });
   });
+
+  app.delete("/users/destroy", (req, res) => {
+    let email = req.body.email;
+
+    jsonfile.readFile(file_path, function (err, content) {
+      for (let i = content.length - 1; i >= 0; i--) {
+        if (content[i].email === email) {
+          console.log("removing " + content[i].email + "from DB");
+          content.pop();
+        }
+      }
+
+      jsonfile.writeFile(file_path, content, function (err) {
+        console.log(err);
+      });
+
+      res.sendStatus(200);
+    });
+  });
+
+  app.put("/user", (req, res) => {
+    let user;
+    let username = req.body.username;
+    let email = req.query.email;
+
+    jsonfile.readFile(file_path, function (err, content) {
+      for (let i = content.length - 1; i >= 0; i--) {
+        if (content[i].email === req.query.email) {
+          console.log(
+            "updated user " +
+              req.query.email +
+              " has now username : " +
+              username
+          );
+
+          user = content[i];
+          user.username = username;
+        }
+      }
+
+      jsonfile.writeFile(file_path, content, function (err) {
+        console.log(err);
+      });
+    });
+    res.send(user);
+  });
+
+  app.get("/user", (req, res) => {
+    let user;
+    let email = req.query.email;
+
+    jsonfile.readFile(file_path, function (err, content) {
+      for (let i = content.length - 1; i >= 0; i--) {
+        if (content[i].email === email) {
+          console.log("found user" + content[i]);
+          console.log(content[i]);
+          user = content[i];
+        }
+      }
+
+      res.send(user);
+    });
+  });
 };
